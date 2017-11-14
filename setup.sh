4de91d8f724ac8024cca14003e1cf6a8ddcdb293
kramsg12 <33532860+kramsg12@users.noreply.github.com>
2017-11-14 18:42:13 +1100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+mkdir git
+cd git 
+git clone https://github.com/EmpireProject/Empire.git
+./Empire/setup/install.sh
+git clone https://github.com/trustedsec/ptf.git
+git clone https://github.com/praetorian-inc/pentestly.git
+./pentestly/install.sh
+git clone https://github.com/4w4k3/Insanity-Framework.git
+./Insanity-Framework/install.sh
